2d9455629d2b5109f590541888feb5c07a815a35
Pendrag00n <88731722+Pendrag00n@users.noreply.github.com>
2023-07-11 01:27:26 +0100

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Variables
+ip='1.1.1.1'
+email_receiver=gmail@gmail.com # Leave empty if not being used
+
+# Check if the IP is either IPv4 or IPv6 and if it's valid
+
+# Check for connectivity and take an action in case of network loss
+
+exit 0
